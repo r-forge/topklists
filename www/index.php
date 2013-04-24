9b34819cf4cf895a40072c34443dbbd7374eb02b
kuglerka (Karl Kugler) <kg.kugler@gmail.com>
2013-04-24 13:52:22 +0000

    updated the website
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/topklists@33 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -1,28 +1,45 @@
-
-<!-- This is the project specific website template -->
-<!-- It can be changed as liked or replaced by other content -->
-
-<?php
-
-$domain=ereg_replace('[^\.]*\.(.*)$','\1',$_SERVER['HTTP_HOST']);
-$group_name=ereg_replace('([^\.]*)\..*$','\1',$_SERVER['HTTP_HOST']);
-$themeroot='http://r-forge.r-project.org/themes/rforge/';
-
-echo '<?xml version="1.0" encoding="UTF-8"?>';
-?>
-<!DOCTYPE html
-	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en   ">
-
-  <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title><?php echo $group_name; ?></title>
-	<link href="<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
-  </head>
-
-<body>
-
-This is a test
-</body>
-</html>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+	<META HTTP-EQUIV="CONTENT-TYPE" CONTENT="text/html; charset=utf-8">
+	<TITLE>TopKLists</TITLE>
+	<META NAME="GENERATOR" CONTENT="LibreOffice 3.5  (Linux)">
+	<META NAME="CREATED" CONTENT="20130104;15093300">
+	<META NAME="CHANGED" CONTENT="20130104;15274800">
+	<link rel="stylesheet" type="text/css" href="incl/main.css">
+</HEAD>
+<BODY LANG="en-US" DIR="LTR">
+<H1>Welcome to the TopKLists page!</H1>
+<P>For multiple full ranked (input) lists representing the same set
+of N objects, the package TopKLists offers 
+</P>
+<OL>
+	<LI><P>statistical inference on the lengths of informative (top-k)
+	partial lists</P>
+	<LI><P>stochastic aggregation of full or partial lists</P>
+	<LI><P>graphical tools for the statistical exploration of input
+	lists, and for aggregation visualization</P>
+</OL>
+<P>The <B>latest stable build</B> can be found on <A HREF="##" TARGET="_blank">CRAN</A>.</P>
+<P>The <STRONG>project summary page</STRONG> you can find <STRONG><A HREF="https://r-forge.r-project.org/projects/topklists/" TARGET="_blank"><SPAN STYLE="text-decoration: none"><SPAN STYLE="font-weight: normal">here</SPAN></SPAN></A></STRONG>.
+</P>
+<P>For a <STRONG>detailed description</STRONG> of the package see the
+<A HREF="##" TARGET="_blank">reference manual</A>.</P>
+<P><B>Reproducible examples</B> can be found in the Vignettes
+(<A HREF="##" TARGET="_blank">TopKSpace</A>, <A HREF="##" TARGET="_blank">TopKInference</A>,
+<A HREF="#" TARGET="_blank">TopKGUI</A>).</P>
+<H2>Citation Information</H2>
+<P>When using TopKLists in your work please refer to it by
+citing:<BR>XXX</P>
+<H2>Examples for the Usage of TopKLists</H2>
+<UL>
+	<LI><P>Link to example paper 1</P>
+	<LI><P>Link to example paper 2</P>
+</UL>
+<H2>Other Links</H2>
+<UL>
+	<LI><P><A HREF="http://www.stat.osu.edu/~statgen/SOFTWARE/TopKCEMC/" TARGET="_blank">TopKCEMC
+	and TopKSpace webpages of Shili Lin</A></P>
+</UL>
+</BODY>
+</HTML>
