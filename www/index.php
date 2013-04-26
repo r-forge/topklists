4b578d0b08a4e0f61f089d800540c462d3c24133
kuglerka (Karl Kugler) <kg.kugler@gmail.com>
2013-04-26 14:27:19 +0000

    updated webpage
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/topklists@39 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -6,7 +6,11 @@
 	<link rel="stylesheet" type="text/css" href="incl/main.css">
 </HEAD>
 <BODY LANG="en-US" DIR="LTR">
+<div style="width: 800px; text-align: center">
+<img src="img/banner.svg" alt="Welcome to the TopKLists website" />
+</div>
 <H1>Welcome to the TopKLists page!</H1>
+<div style="max-width:750px">
 <P>For multiple full ranked (input) lists representing the same set
 of N objects, the package TopKLists offers 
 </P>
@@ -18,21 +22,26 @@ of N objects, the package TopKLists offers
 	lists, and for aggregation visualization</P>
 </OL>
 <P>The <B>latest stable build</B> can be found on <A HREF="##" TARGET="_blank">CRAN</A>.</P>
-<P>The <STRONG>project summary page</STRONG> you can find <STRONG><A HREF="https://r-forge.r-project.org/projects/topklists/" TARGET="_blank"><SPAN STYLE="text-decoration: none"><SPAN STYLE="font-weight: normal">here</SPAN></SPAN></A></STRONG>.
+<P>You can find the <STRONG>latest news</STRONG> and the <STRONG>project summary page</STRONG> <STRONG><A HREF="https://r-forge.r-project.org/projects/topklists/" TARGET="_blank"><SPAN STYLE="text-decoration: none"><SPAN STYLE="font-weight: normal">here</SPAN></SPAN></A></STRONG>.
 </P>
 <P>For a <STRONG>detailed description</STRONG> of the package see the
 <A HREF="##" TARGET="_blank">reference manual</A>.</P>
-<P><B>Reproducible examples</B> can be found in the Vignettes
+<P>When using TopKLists for the first time it's helpful to make use of <B>reproducible examples</B>, which can be found in the Vignettes
 (<A HREF="##" TARGET="_blank">TopKSpace</A>, <A HREF="##" TARGET="_blank">TopKInference</A>,
 <A HREF="#" TARGET="_blank">TopKGUI</A>).</P>
+</div>
 <H2>Citation Information</H2>
+<div style="max-width:750px">
 <P>When using TopKLists in your work please refer to it by
 citing:<BR>XXX</P>
+</div>
 <H2>Examples for the Usage of TopKLists</H2>
+<div style="max-width:750px">
 <UL>
-	<LI><P>Link to example paper 1</P>
-	<LI><P>Link to example paper 2</P>
+  <li><b>Package “TopKLists” for rank-based genomic data integration</b>
+    <br/>M Schimek, E Budinska, K Kugler, S Lin, Proc IASTED CompBio, 434-440</li>
 </UL>
+</div>
 <H2>Other Links</H2>
 <UL>
 	<LI><P><A HREF="http://www.stat.osu.edu/~statgen/SOFTWARE/TopKCEMC/" TARGET="_blank">TopKCEMC
