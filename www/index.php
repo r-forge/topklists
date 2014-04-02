cc7503dbc1ca1cc8f2eda2d1ae99e6fa56573bff
svendula (Vendula Svendova) <svendula@gmail.com>
2014-04-02 11:56:01 +0000

    update webpage
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/topklists@167 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -7,40 +7,48 @@
 </HEAD>
 <BODY LANG="en-US" DIR="LTR">
 <div style="width: 800px; text-align: center">
-<img src="img/banner.svg" alt="Welcome to the TopKLists website" />
-</div>
 <H1>Welcome to the TopKLists page!</H1>
+<img src="img/website_pic.png" alt="Welcome to the TopKLists website"  height="20%" />
+</div>
+<H1>Details</H1>
 <div style="max-width:750px">
-<P>For multiple full ranked (input) lists representing the same set
-of N objects, the package TopKLists offers 
+<P>For multiple ranked input lists (full or partial) representing the same set
+of N objects, the package <strong>Top<font color="FireBrick">K</font>Lists</strong> offers 
 </P>
 <OL>
-	<LI><P>statistical inference on the lengths of informative (top-k)
-	partial lists</P>
+	<LI><P>statistical inference on the lengths of informative top-k
+	lists</P>
 	<LI><P>stochastic aggregation of full or partial lists</P>
 	<LI><P>graphical tools for the statistical exploration of input
-	lists, and for aggregation visualization</P>
+	lists, and for the visualization of aggregation results</P>
 </OL>
 <P>The <B>latest stable build</B> can be found on <A HREF="##" TARGET="_blank">CRAN</A>.</P>
 <P>You can find the <STRONG>latest news</STRONG> and the <STRONG>project summary page</STRONG> <STRONG><A HREF="https://r-forge.r-project.org/projects/topklists/" TARGET="_blank"><SPAN STYLE="text-decoration: none"><SPAN STYLE="font-weight: normal">here</SPAN></SPAN></A></STRONG>.
 </P>
 <P>For a <STRONG>detailed description</STRONG> of the package see the
 <A HREF="##" TARGET="_blank">reference manual</A>.</P>
-<P>When using TopKLists for the first time it's helpful to make use of <B>reproducible examples</B>, which can be found in the Vignettes
-(<A HREF="##" TARGET="_blank">TopKSpace</A>, <A HREF="##" TARGET="_blank">TopKInference</A>,
-<A HREF="#" TARGET="_blank">TopKGUI</A>).</P>
+<P>When using TopKLists for the first time it's helpful to make use of <B>reproducible examples</B>, which can be found in the <A HREF="##" TARGET="_blank">Vignette</A> 
+(comprises the modules TopKInference, TopKSpace, TopKGraphics and TopKListsGUI).</P>
 </div>
 <H2>Citation Information</H2>
 <div style="max-width:750px">
 <P>When using TopKLists in your work please refer to it by
-citing:<BR>Hall, P. and Schimek, M. G. (2012).  Moderate deviation-based inference for random degeneration in paired rank lists. J. Amer. Statist. Assoc., 107, 661-672.</P>
-<BR>Lin, S. and Ding, J. (2009). Integration of ranked lists via Cross Entropy Monte Carlo with applications to mRNA and microRNA studies. Biometrics, 65, 9-18.</P>
+citing:
+<UL>
+	<li>Hall, P. and Schimek, M. G. (2012).  Moderate deviation-based inference for random degeneration in paired rank lists. J. Amer. Statist. Assoc., 107, 661-672.</li>
+<BR><li>Lin, S. and Ding, J. (2009). Integration of ranked lists via Cross Entropy Monte Carlo with applications to mRNA and microRNA studies. Biometrics, 65, 9-18.</li>
+</UL>
 </div>
-<H2>Examples for the Usage of TopKLists</H2>
+<H2>Examples for the Usage</H2>
 <div style="max-width:750px">
 <UL>
-  <li><b>Package “TopKLists” for rank-based genomic data integration</b>
-    <br/>M Schimek, E Budinska, K Kugler, S Lin, Proc IASTED CompBio, 434-440</li>
+  <li>Package “TopKLists” for rank-based genomic data integration
+    <br/>Schimek, M. G., Budinska, E., Kugler, K. and Lin, S. (2011), Proc IASTED CompBio, 434-440.</li>
+  <li>Modelling the rank order of Web search engine results
+	<br/>Schimek, M. G., Bloice, M. (2012), Proc IWSM, 303-308.</li>
+  <li>An inference and integration approach for the consolidation of ranked lists
+	<br/>Schimek, M. G., Mysickova, A. and Budinska, E. (2010), J. Statist. Plan. Inference, Vol 47, 1152-1166.
+	</li>
 </UL>
 </div>
 <H2>Other Links</H2>
